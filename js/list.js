5e37e816c6216d4382cd27c1ddec931145da20a0
Lincoln <lincoln@Katies-MacBook-Pro-2.local>
2016-07-17 14:30:40 -0500

    Changes to shopping list

--- js/list.js
+++ js/list.js
@@ -1,63 +1,60 @@
-var itemInput = document.getElementById("new-item");
-var costInput = document.getElementById("cost");
-var addButton = document.getElementsByTagName("button")[0];
+var itemInput = document.getElementById("newItem");
+var costInput = document.getElementById("costEstimate");
+var addItemButton = document.getElementById("addItemButton");
+// var addButton = document.getElementsByTagName("button")[0];
 var listBuild = document.getElementById("shoppingList");
-var button = document.getElementById("addItemButton");
+var totalPrice = document.getElementById("totalCost");
+var total = myarr.reduce(function(a, b) {
+  return a + b;
+}, 0);
 
+// function addTo() {
+//    myarr.push(document.getElementById("costEstimate").value);
+//    console.log(myarr); //to confirm it has been added to the array
+// }
 
-// add a new item
+// VARIABLE TO CONVERT ARRAY TO NUMBER (or function??)
+// Function TO TOTAL ARRAY
+// FUNCTION TO ADD TOTAL TO #totalCOST SOMEHOW ON CLICK.... HMMMM...
 
-var createNewItemElement = function(itemString) {
-    var listItem = document.createElement("li");
-    var deleteButton = document.createElement("button");
-
-    // Each element needs appending
- listItem.appendChild()
-}
 
 var addItem = function() {
-
 var item = itemInput.value;
 var cost = costInput.value;
 var li = document.createElement("li");
-li.innerHTML = item + ": " + cost;
+li.innerHTML = item + ": " + "$" + cost;
+console.log(cost);
+listBuild.appendChild(li);
+var p = document.createElement("p");
+p.innerHTML = "$ " + cost;
+totalPrice.appendChild(p);
+myarr.push(costInput.value);
+itemInput.value = " ";
+costInput.value = null;
+itemInput.focus();
+arrToIntegers(myarr);
+// totalCost(myarr);
+console.log(total);
 
-shoppingList.appendChild(li);
+// costInput.value = " ";
+}
+
+var myarr = [];
+
+function arrToIntegers() {
+for (var i=0; i<myarr.length; i++) {
+    myarr[i] = parseInt(myarr[i], 10);
+    console.log(myarr);
+}
 }
 
 
-    // when button is pressed
-        // create new list item with the text from #new-item and estimated cost from #cost
-            // label
-            // button.delete
-
-        // organize into categories
-
-
-// organize by category
-
-
-
-
-// delete existing item
-var deleteItem = function() {
-
-
-console.log("delete task...");
-
-}
-
-
-
-// total estimated cost
-var totalCost = function() {
-console.log("Total cost...");
-// for each item added to list
-// add estimated cost
-
-for (var i=0; i < costInput.length; i++)
-+=costInput;
-
-}
 
 addItemButton.onclick = addItem;
+
+
+function totalCost() {
+for ( var i = 0; i < myarr.length; i++ ){
+ total + myarr[i];
+ }
+ }
