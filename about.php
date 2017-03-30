665173bfe27daab5f8d01421eb75ee4c95cec51a
Lincoln Wisely <lincolnwisely@gmail.com>
2017-03-30 16:29:15 -0500

    Update information

--- about.php
+++ about.php
@@ -8,13 +8,8 @@ include ('inc/header.php');
     <div class="meat clearfix">
   	   <div class="col primary">
   		<h2>About</h2>
-      <p>When I'm not working, studying or overthinking my life trajectory, I invest time and energy on the things I love the most: My girlfriend, Arin, and my dogs, Tony and Vince. Hobbies include traveling, trying new restaurants (and reveling in old favorites), taking photos, listening to music, watching movies, sipping bourbon, sipping tea, reading books, learning new things and curbing my perpetual sweet tooth. I'm reserved, creative, neurotic and curious.</p>
-
-      <br>
-      <p>My current efforts on the front end side include learning PHP, JavaScript and MySQL, in addition to refining my knowledge of HTML, CSS, Sass and git. I've surprisingly grown quite fond of the terminal, so let's see where this goes.</p>
+      <p>When I'm not working, studying or overthinking my life trajectory, I spend as much time I can with my fiance, Arin, and our dogs, Tony and Vince. A few of my favorite things: travel, photography, music, movies, books and home projects.</p>
         <br>
-      <p>Many people know me by the name of Emily, but I also go by Lincoln – particularly in coding environments.</p>
-
     </div>
   </div>
 <?php
